75a1d9d01434b0c053ad28a7a09ece1681eff16f
dkonst001 <danny.konstadt@gmail.com>
2015-01-24 16:49:54 +0200

    test

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file incudes functionality to inverse 2x2 matrix and use caching 
+## mechanizem
+ 
 
-## Write a short comment describing this function
+## makeCacheMatrix function include function templates for inversing matrix and
+## storing the original and inversed matrix in cach. The Caching mechanizem is 
+## actually the environment created for each matrix instance.
 
 makeCacheMatrix <- function(x = matrix()) {
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(xIn) inv <<- xIn
+        getInverse <- function() inv
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 
 }
 
@@ -12,4 +25,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInverse(inv)
 }
